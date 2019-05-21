d97ce278ba557cc09071964b2238614122e6691b
Haifaasalsaifa <haifaasalsaifa14@gmail.com>
2019-05-21 11:24:42 +0700

    Haifa Salsabila

--- hr_wiki/urls.py
+++ hr_wiki/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='wiki-home')
+    path('', views.home, name='wiki-home'),
+    path('search/', views.sear, name='wiki-search')
+
 ]
--- hr_wiki/views.py
+++ hr_wiki/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'hr_wiki/home.html')
+def sear(request):
+    return render(request, 'hr_wiki/search.html',{'name' :'search'})
