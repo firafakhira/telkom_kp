88fbc3d8070e1b6d9295b0f403713b93c8780a23
akbar <jamalakbarlam@gmail.com>
2019-05-21 14:53:33 +0700

    rapihin 2.0

--- hr_wiki/urls.py
+++ hr_wiki/urls.py
@@ -3,12 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='wiki-home'),
-<<<<<<< HEAD
-    path('search/', views.sear, name='wiki-search')
-    
-=======
     path('home2/', views.home2, name='wiki-home2'),
     path('search/', views.sear, name='wiki-search')
 
->>>>>>> 0b8a0203fee861eb23577fa5851df7e1b4f179fa
 ]
