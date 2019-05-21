2a98fd8a999db03ccbaa478ac1adc728649b9316
Fauzan Firdaus <faoezanfirdaoes@gmail.com>
2019-05-21 10:54:26 +0700

    Fauzan
    Penambahan : home2.css, home2.png, home2.html dan penambahan code di base.html, urls.py dan views.py

--- hr_wiki/views.py
+++ hr_wiki/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'hr_wiki/home.html')
+def home2(request):
+    return render(request, 'hr_wiki/home2.html',{'name': 'home2'})
