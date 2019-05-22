069bfb2efb4d34d3d3a5c3b35a015031888c22f2
Haifaasalsaifa <haifaasalsaifa14@gmail.com>
2019-05-22 09:08:00 +0700

    dajarin akbar

--- hr_wiki/urls.py
+++ hr_wiki/urls.py
@@ -3,13 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='wiki-home'),
-<<<<<<< HEAD
-    path('search/', views.sear, name='wiki-search'),
-    path('home2/', views.home2, name='wiki-home2'),
-    path('search/', views.sear, name='wiki-search')
-=======
     path('home2/', views.home2, name='wiki-home2'),
     path('search/', views.sear, name='wiki-search')
 
->>>>>>> 88fbc3d8070e1b6d9295b0f403713b93c8780a23
 ]
